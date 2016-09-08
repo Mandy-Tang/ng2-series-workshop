6edea289abc11bfbbe1690b1922238c087cae332
mandy <hitangman@qq.com>
2016-09-08 17:35:35 +0800

    add a input

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 import {bootstrap} from '@angular/platform-browser-dynamic'
+import {disableDeprecatedForms, provideForms} from '@angular/forms'
 import {App} from './app'
-bootstrap(App)
+bootstrap(App, [disableDeprecatedForms(), provideForms()])
