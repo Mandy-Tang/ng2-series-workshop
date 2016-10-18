dd5190f81d106a5307bfae03e17adbd3a6432906
mandy <hitangman@qq.com>
2016-10-18 16:48:29 +0800

    bootstrap service and corret mistakes in last commit

--- src/app/shared/services/api.ts
+++ src/app/shared/services/api.ts
@@ -1,14 +1,14 @@
 import {Response, Http, Headers} from '@angular/http';
-import {Injectable} form '@angular/core';
-import {Observable} form 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
-@Injectable
+@Injectable()
 export class ApiService {
   api_url: string = 'http://localhost:3500';
-  headers: Headers = new Headers {
+  headers: Headers = new Headers ({
     'Content-Type': 'application/json',
     'Accept': 'application/json'
-  }
+  });
 
   constructor(private http: Http) {}
 
@@ -26,7 +26,7 @@ export class ApiService {
   }
 
   get(path: string): Observable<any> {
-    return this.http.get('${this.api_url}${path}', {
+    return this.http.get(`${this.api_url}${path}`, {
       headers: this.headers
     }).map(this.checkError)
     .catch(err => Observable.throw(err))
@@ -34,13 +34,11 @@ export class ApiService {
   }
 
   post(path: string, body): Observable<any> {
-    return this.http.post('${this.api_url}${path}', JSON.stringify(body), {
+    return this.http.post(`${this.api_url}${path}`, JSON.stringify(body), {
       headers: this.headers
     }).map(this.checkError)
     .catch(err => Observable.throw(err))
-    .map(this.getJson)
+    .map(this.getJson);
   }
-
-
 
 }
