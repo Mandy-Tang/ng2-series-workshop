dad29a14c4a760fdeb97f83ca18f93b5c5898cf3
mandy <hitangman@qq.com>
2016-10-24 15:01:55 +0800

    restructure the app-bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {Home} from './home';
+import {AppBar} from './shared/components';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'app',
   template: require('./app.html'),
-  directives: [...ROUTER_DIRECTIVES]
+  directives: [...ROUTER_DIRECTIVES, AppBar]
 })
 
 export class App{}
--- src/app/shared/components/app-bar/index.ts
+++ src/app/shared/components/app-bar/index.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'app-bar',
   template: require('./app-bar.html'),
+  directives: [
+    ...ROUTER_DIRECTIVES
+  ],
   styles: [require('./app-bar.css')]
 })
 
