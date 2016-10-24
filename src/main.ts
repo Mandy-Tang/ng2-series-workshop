2cb59b24dd23d8ef34800819c88609574f233cc8
mandy <hitangman@qq.com>
2016-10-24 14:42:48 +0800

    add router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {disableDeprecatedForms, provideForms} from '@angular/forms';
-import {App, providers} from './app';
+import {App, providers, router} from './app';
 import {HTTP_PROVIDERS} from '@angular/http';
+import {provideRouter} from '@angular/router';
 
 bootstrap(App, [
   ...HTTP_PROVIDERS,
   disableDeprecatedForms(),
   provideForms(),
+  provideRouter(router),
   ...providers
 ]);
