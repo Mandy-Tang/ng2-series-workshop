ebe4a85b0ec9d99e1019498fec86114d623cbf56
mandy <hitangman@qq.com>
2016-10-24 18:10:08 +0800

    add canActivate in main part

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,10 +3,18 @@ import {Home} from './home';
 import {About} from './about';
 import {AuthCompoment} from './auth';
 import {MainComponent} from './main';
+import {AuthGuard} from './shared/services';
 
 export const router: RouterConfig = [
-  {path: '', component: Home},
-  {path: 'about', component: About},
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {path: '', component: Home},
+      {path: 'about', component: About}
+    ]
+  },
   {path: 'auth', component: AuthCompoment},
   {path: '**', redirectTo: ''}
 ];
