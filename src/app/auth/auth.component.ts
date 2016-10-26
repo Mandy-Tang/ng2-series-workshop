36df250f805d2a543fedbe758b807987b4b8895a
mandy <hitangman@qq.com>
2016-10-26 16:12:55 +0800

    add mode, init authenticate feature in auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../shared/services';
+import {Router} from '@angular/router';
+
+const linkTexts = {
+  signin: 'Don\'t have an account?',
+  signup: 'Already have an account?'
+}
 
 @Component({
   selector: 'auth',
@@ -7,6 +14,30 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class AuthCompoment implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  user: {email: string, password: string};
+  mode: 'signin' | 'signup';
+  linkText: string;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.user = {email: '', password: ''};
+    this.mode = 'signin';
+    this.linkText = linkTexts.signin;
+  }
+
+  changeMode(): void {
+    if (this.mode === 'signin') {
+      this.mode = 'signup';
+      this.linkText = linkTexts.signup;
+    } else {
+      this.mode = 'signin';
+      this.linkText = linkTexts.signin;
+    }
+  }
+
+  authenticate(): void {
+    this.authService.ahthenticate(this.mode, this.user)
+      .subscribe(() => this.router.navigate(['/']))
+  }
 }
