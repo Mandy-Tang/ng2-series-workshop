cdb8a0e569d2ffc872fd2f6625fb5f4805bc7a82
mandy <hitangman@qq.com>
2016-10-26 15:51:44 +0800

    change authService name

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,13 +3,13 @@ import {Home} from './home';
 import {About} from './about';
 import {AuthCompoment} from './auth';
 import {MainComponent} from './main';
-import {AuthGuard} from './shared/services';
+import {AuthService} from './shared/services';
 
 export const router: RouterConfig = [
   {
     path: '',
     component: MainComponent,
-    canActivate: [AuthGuard],
+    canActivate: [AuthService],
     children: [
       {path: '', component: Home},
       {path: 'about', component: About}
